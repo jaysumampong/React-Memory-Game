9e4d5eb36493d242da4633e2e022494554275cc6
Jay Sumampong <jaysumampong@gmail.com>
2019-09-28 15:11:29 -0600

    wrote a few files but their is an error

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,26 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import ImageCard from "./components/ImageCards";
+import Wrapper from "./components/Wrapper";
+import NavBar from "./components/NavBar";
+import cards from "./cards.json.";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  // Setting this.state.friends to the friends json array
+  state = {
+    Image
+  };
+
+
+  // Map over this.state.friends and render a FriendCard component for each friend object
+  render() {
+    return (
+      <Wrapper>
+        <NavBar>test</NavBar>
+        {this.state.image.map(image => (
+          <ImageCard
+            id={image.id}
+            key={image.id}
+            image={image.Image}
+          />
+        ))}
+      </Wrapper>
+    );
+  }
 }
 
 export default App;
---
+++ my-app/src/components/NavBar/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./style.css";
+
+function NavBar(props) {
+  return (
+    <nav>
+
+    </nav>
+  )
+}
+
+export default NavBar;
