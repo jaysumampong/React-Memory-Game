8b9b6d51fd164ec88433f1b3f2adbb85252c46e6
Jay Sumampong <jaysumampong@gmail.com>
2019-10-25 15:45:49 -0600

    Updates

---
+++ precache-manifest.cae3c5a2ed2db50f004f1d51edc2ebee.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7395fdf59daca66d11152aa411ee3f5",
+    "url": "/React-Memory-Game/index.html"
+  },
+  {
+    "revision": "52931dbb1a2b0038229f",
+    "url": "/React-Memory-Game/static/css/main.5ec87d05.chunk.css"
+  },
+  {
+    "revision": "5962eb9164ca1c6b6712",
+    "url": "/React-Memory-Game/static/js/2.1b57c531.chunk.js"
+  },
+  {
+    "revision": "52931dbb1a2b0038229f",
+    "url": "/React-Memory-Game/static/js/main.ced06c25.chunk.js"
+  },
+  {
+    "revision": "3af323128c2f9778490e",
+    "url": "/React-Memory-Game/static/js/runtime-main.57166016.js"
+  }
+]);
